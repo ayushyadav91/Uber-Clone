ddc08506a339b506374317ec8247cbe6f9f7cc2e
theayush434 <theayush434@gmail.com>
2024-12-10 10:08:17 +0530

    captain login, logout and profile routes done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const captainModel = require('../models/captain.model');
 
 
 
@@ -20,6 +22,27 @@ module.exports.authUser = async (req ,res, next)=>{
           return next();
      
      }catch(err){
+          return res.status(401).json({message:'Unauthorized'});
+     }
+}
+
+module.exports.authCaptain = async (req ,res, next)=>{
+     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+     if(!token){
+          return res.status(401).json({message:'Unauthorized'});
+     }
+     const blacklistToken = await blacklistTokenModel.findOne({token:token});
+     if(blacklistToken){
+          return res.status(401).json({message:'Unauthorized'});
+     }
+     try{
+          const decoded = jwt.verify(token,process.env.JWT_SECRET);
+          const captain = await captainModel.findById(decoded._id);
+
+          req.captain = captain;
+          return next();
+     
+     }    catch(err){
           return res.status(401).json({message:'Unauthorized'});
      }
 }
